7bff373113a4dfe40576c40f248a173792cc6c6e
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-09-22 06:51:48 +0200

    Add project files.

---
+++ ClockV20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+#pragma warning disable 4014
+
+namespace ClockV20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PulseGenerator theGenerator = new PulseGenerator();
+
+            // Create some Clock objects
+            Clock c1 = new Clock("Klokken er: ");
+            theGenerator.Pulse += c1.Tick;
+            theGenerator.Pulse += c1.PrintTime;
+            
+
+            // Attach the relevant methods from the Clock objects 
+            // to the Pulse event in theGenerator
+
+
+            // Start pulsing...
+            theGenerator.Start(200);
+
+            Console.ReadKey();
+        }
+    }
+}
